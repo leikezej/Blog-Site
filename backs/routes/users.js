2cd470c64ef760ad4f399553a37d6b6f2fbf44f7
jep zekiel <jezedevkiel21@gmail.com>
2022-10-29 00:47:00 +0800

    Update users.js

--- backs/routes/users.js
+++ backs/routes/users.js
@@ -1,7 +1,26 @@
-import express from "express"
+// import express from "express"
 
-const router = express.Router()
+// const router = express.Router()
 
-//TODO
+// //TODO
 
-export default router
+// export default router
+
+import express from "express";
+import {
+    getUsers, 
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser
+} from "../controllers/UserController.js";
+ 
+const router = express.Router();
+ 
+router.get('/users', getUsers);
+router.get('/users/:id', getUserById);
+router.post('/users', createUser);
+router.patch('/users/:id', updateUser);
+router.delete('/users/:id', deleteUser);
+ 
+export default router;
